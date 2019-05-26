111993f0af0a85c1b9296e65c22c018a5e6bda12
leehyunjun <zxzxs9182@gmail.com>
2019-05-26 15:57:22 +0900

    call city weather use API

--- src/Component/Weather/index.js
+++ src/Component/Weather/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import { Switch, Route} from 'react-router';
 
 const API_CITIES ="http://localhost:8080/weather-crawler/available-cities";
+const API_WEATHER = 'http://localhost:8080/weather-crawler/current-weathers/by-city-name';
+
+
 class Weather extends React.Component{
     state = {
             cities : null
@@ -22,7 +25,7 @@ class Weather extends React.Component{
     render(){
         //const foo = this.state.foo;
         const { cities } = this.state;
-
+        const { match } = this.props;
 
         if(!cities){
             return <div>Loading....</div>;
@@ -30,13 +33,56 @@ class Weather extends React.Component{
 
         return (
             <div>
-                <ul>
-                {cities.map(item =>{
-                    return <li key={item}>{item}</li>;
-                })}
-                </ul>
+               <Switch>
+                         <Route path={`${match.path}/:cityId`} component={SelectedWeather} />
+                         <Route exact path={match.path} render={ ()=> cities.map(item => { return <p> <a class="cityName" href={match.path+"/"+item}>{item}</a></p>;}) } />} />
+                       </Switch>
             </div>
         );
+    }
+}
+
+class SelectedWeather extends React.Component {
+    state = {
+      weather: null
+    };
+
+    async componentDidMount() {
+      const { cityId } = await this.props.match.params;
+      const api = `${API_WEATHER}/${cityId}`;
+
+      const weather = await fetch(api)
+        .then(res => res.json())
+        .then(data => data);
+
+      this.setState({
+        weather
+      });
+    }
+
+    render() {
+      const { cityId } = this.props.match.params;
+
+      const { weather } = this.state;
+
+      if (!weather) {
+      return <div>Loading...</div>;
+      }
+
+      const celsius = (weather.main.temp - 273.15).toFixed(2); // kelvin to celsius
+      const weatherMain = weather.weather[0].main;
+      const iconId = weather.weather[0].id;
+
+      return (
+        <div className="weather-today">
+          <h2 className="weather-city">{cityId}</h2>
+
+          <div className="weather-today-meta">
+            <h3 className="weather-main">{weatherMain}</h3>
+            <div className="weather-temp">{celsius}°</div>
+          </div>
+        </div>
+      );
     }
 }
 export default Weather
