adb1becbe4a630e11ed272dc7a8f5c8c5a4a9870
yogjin <youngjin988@gmail.com>
2019-05-27 01:28:50 +0900

    add mist&rain image

--- src/Component/Weather/index.js
+++ src/Component/Weather/index.js
@@ -5,6 +5,8 @@ import hazeimage from './haze.jpg';
 import clearimage from './clear.JPG';
 import cloudimage from './cloud.JPG';
 import fogimage from './fog.jpg';
+import rainimage from './rain.jpg';
+import mistimage from './mist.jpg';
 const API_CITIES ="http://localhost:8080/weather-crawler/available-cities";
 const API_WEATHER = 'http://localhost:8080/weather-crawler/current-weathers/by-city-name';
 
@@ -90,6 +92,12 @@ class SelectedWeather extends React.Component {
        else if( weatherMain=="Fog"){
               weatherimage=fogimage;
               }
+       else if( weatherMain=="Rain"){
+             weatherimage=rainimage;
+       }
+       else if( weatherMain=="Mist"){
+             weatherimage=mistimage;
+       }
 
        if(celsius<0){
                comment = <div> 엄청 추운 날씨입니다. 꽁꽁 싸메고 가세요~!</div>;
