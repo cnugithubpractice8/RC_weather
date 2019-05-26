1eaed2fd0b8f385d61fd906beaba1a2bcce71247
Nroot33 <jessica0533@gmail.com>
2019-05-26 16:43:51 +0900

    add information

--- src/About.js
+++ src/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import profile from './profile.png';
 
 // Stateless component
 const Home = () => {
@@ -8,6 +9,8 @@ const Home = () => {
       <a href= "https://github.com/Nroot33">201502049 노효근 </a><br></br><br></br>
       <a href= "https://github.com/yogjin"> 201702015 박영진 </a><br></br><br></br>
       <a href= "https://github.com/hjlee9182"> 201502099 이현준 </a><br></br><br></br>
+      <img src={profile} alt="profile"/>
+      <h1>Welcome EveryOne</h1>
       <a href= "https://www.twitch.tv/noocutt33"> NooCuTT Twitch channel </a><br></br><br></br>
 
     </div>
